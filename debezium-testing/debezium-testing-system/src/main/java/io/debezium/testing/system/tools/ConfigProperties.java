701615df9730155388ecdc4b997521577d8cf1f9
Vojtech Juranek <vjuranek@redhat.com>
2026-09-23 09:40:59 +0200

    Fix CSV version Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- debezium-testing/debezium-testing-system/src/main/java/io/debezium/testing/system/tools/ConfigProperties.java
+++ debezium-testing/debezium-testing-system/src/main/java/io/debezium/testing/system/tools/ConfigProperties.java
@@ -66,7 +66,7 @@ public final class ConfigProperties {
     public static final boolean FORCE_KRAFT = booleanProperty("test.force.kraft", true);
 
     // Apicurio Registry configuration
-    public static final String VERSION_APICURIO = System.getProperty("version.apicurio", "3.2.5");
+    public static final String VERSION_APICURIO = System.getProperty("version.apicurio", "3.3.1-r1");
     public static final String APICURIO_OPERATOR_NAME = System.getProperty("test.apicurio.operator.name", "apicurio-registry-3");
     public static final String APICURIO_OPERATOR_CHANNEL = System.getProperty("test.apicurio.operator.channel", "3.x");
     public static final String APICURIO_OPERATOR_VERSION = System.getProperty("test.apicurio.operator.version", VERSION_APICURIO);
